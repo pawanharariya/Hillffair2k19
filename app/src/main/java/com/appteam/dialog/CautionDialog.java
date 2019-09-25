0eebdf94cdff1874e61e5cc5bdc393534c10d260
pawanharariya <pawanharariya@gmail.com>
2019-09-26 02:10:15 +0530

    Profile Functional

---
+++ app/src/main/java/com/appteam/dialog/CautionDialog.java
@@ -0,0 +1,45 @@
+package com.appteam.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.appteam.hillfair2k19.R;
+
+
+public class CautionDialog extends Dialog {
+
+    TextView textView;
+
+    public CautionDialog(@NonNull Activity context) {
+        super(context);
+        initUI();
+    }
+
+    public void initUI() {
+        setContentView(R.layout.activity_caution_dialog);
+        setCancelable(true);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.dimAmount = 0.3f;
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getWindow().setAttributes(lp);
+        setCanceledOnTouchOutside(true);
+        getWindow().setGravity(Gravity.CENTER);
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        findViewById(R.id.proceed).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+}
